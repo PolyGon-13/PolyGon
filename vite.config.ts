a957d1fbd4a9b40299cbd29a14d4b94efbd24978
PolyGon-13 <hyundiego@hanyang.ac.kr>
2026-03-19 00:32:07 +0900

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // for github pages deployment
+  base: '/PolyGon/', // Explicit repository name for GitHub Pages
 })
